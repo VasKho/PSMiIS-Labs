8eadbaea14f9038fa5b81a0f6d042991818d7cf4
VasKho <vasya.khoroshavin@gmail.com>
2022-03-11 21:51:26 +0300

    Add visualization

--- Lab_1/divide_without_remaining.c
+++ Lab_1/divide_without_remaining.c
@@ -35,29 +35,89 @@ int divide_without_remaining(int divident, int divider)
     int shifted_divider = divider << shift;
     int remaining = divident;
     int result = 0x00;
-    printf("Tact 0\n");
-    print_binary(remaining);
-    print_binary(shifted_divider);
     remaining -= shifted_divider;
     if (remaining < 0x00) result |= 0x00;
     else result |= 0x01;
-    print_binary(remaining);
-    printf("result: ");
-    print_binary(result);
     for(int i = 0x00; i < shift; ++i)
     {
-        printf("Tact %d\n", i+1);
-        print_binary(remaining);
-        print_binary(shifted_divider);
         remaining <<= 0x01;
         result <<= 0x01;
         if (remaining < 0) remaining += shifted_divider;
         else remaining -= shifted_divider;
         if (remaining < 0x00) result |= 0x00;
         else result |= 0x01;
+    }
+    return result;
+}
+
+int divide_without_remaining_visual(int divident, int divider)
+{
+    printf("  ");
+    print_binary(divident);
+    printf("/ ");
+    print_binary(divider);
+    printf("\n");
+    if (divider == 0x00)
+    {
+        printf("Error! Division by zero.\n");
+        exit(1);
+    }
+    int shift = binary_size(divident) - binary_size(divider);
+    int tact = 0;
+    if (shift < 0) return 0x00;
+    int shifted_divider = divider;
+    printf("Binary shift\n");
+    for(int i = 0; i < shift; ++i)
+    {
+        printf("Tact %d\n", tact++);
+        print_binary(shifted_divider);
+        printf("\n");
+        shifted_divider <<= 1;
+    }
+    printf("Tact %d\n", tact++);
+    print_binary(shifted_divider);
+    printf("\n");
+    int remaining = divident;
+    int result = 0x00;
+    printf("Subtraction with shift\n");
+    printf("Tact %d (divident - shifted_divider)\n  ", tact++);
+    print_binary(remaining);
+    printf("- ");
+    print_binary(shifted_divider);
+    printf("= ");
+    remaining -= shifted_divider;
+    print_binary(remaining);
+    printf("\n");
+    if (remaining < 0x00) result |= 0x00;
+    else result |= 0x01;
+    printf("Tact %d (add digit to result)\n", tact++);
+    printf("result: ");
+    print_binary(result);
+    printf("\n");
+    for(int i = 0x00; i < shift; ++i)
+    {
+        remaining <<= 0x01;
+        printf("Tact %d (remaining shift)\n", tact++);
         print_binary(remaining);
-        printf("result: ");
+        printf("\n");
+        result <<= 0x01;
+        printf("Tact %d (result shift)\nresult: ", tact++);
         print_binary(result);
+        printf("\n");
+        printf("Tact %d (remaining - shifted_divider)\n  ", tact++);
+        print_binary(remaining);
+        printf("- ");
+        print_binary(shifted_divider);
+        printf("= ");
+        if (remaining < 0) remaining += shifted_divider;
+        else remaining -= shifted_divider;
+        print_binary(remaining);
+        printf("\n");
+        if (remaining < 0x00) result |= 0x00;
+        else result |= 0x01;
+        printf("Tact %d (add digit to result)\nresult: ", tact++);
+        print_binary(result);
+        printf("\n");
     }
     return result;
 }
--- Lab_1/divide_without_remaining.h
+++ Lab_1/divide_without_remaining.h
@@ -7,3 +7,5 @@ void print_binary(int);
 int binary_size(int);
 
 int divide_without_remaining(int divident, int divider);
+
+int divide_without_remaining_visual(int divident, int divider);
